ca03923e5aa3793868fbd2e06c24f42fcbaf936f
marco-lacal <marco.lacal@knights.ucf.edu>
2022-10-30 12:19:54 -0400

    forget navmesh, two points
    how do u get the damn position in this engine

--- enemy/EnemyStates/PatrolState.gd
+++ enemy/EnemyStates/PatrolState.gd
@@ -1,17 +1,24 @@
 extends "res://enemy/State.gd"
 
-var target_point := Node
+export var target_1 := NodePath()
+export var target_2 := NodePath()
+
+onready var next_target := get_node(target_2)
 
 # Called when the node enters the scene tree for the first time.
 func enter():
 	print("hello from patrol")
 	
-	#for i in targets_array.size():
-	#	print(targets_array[i])
+	print(target_1, target_2)
+	
+	if next_target.name == "Pos-2":
+		next_target = get_node(target_1)
+	else:
+		next_target = get_node(target_2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func update(delta):
 	#use navmesh to go to a random point
-	 
+	print(next_target.get_global_transform().get_translation())
 	pass
